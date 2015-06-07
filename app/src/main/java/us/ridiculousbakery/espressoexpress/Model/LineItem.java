21ae6d85e4b51aa9753032ae590b8b2cecbe8396
Teddy Wyly <wylynout@gmail.com>
2015-06-07 18:56:07 -0400

    dialog communication pattern

--- app/src/main/java/us/ridiculousbakery/espressoexpress/Model/LineItem.java
+++ app/src/main/java/us/ridiculousbakery/espressoexpress/Model/LineItem.java
@@ -4,7 +4,12 @@ package us.ridiculousbakery.espressoexpress.Model;
  * Created by bkuo on 6/3/15.
  */
 public class LineItem {
+
     private Item  item;
     private ItemOption itemOption;
+
+    public LineItem(Item item) {
+        this.item = item;
+    }
 
 }
