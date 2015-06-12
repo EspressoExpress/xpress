5deb472e6be445187ce7e98b25981e60713d46c7
Teddy Wyly <wylynout@gmail.com>
2015-06-12 03:31:32 -0400

    parse working

---
+++ app/src/main/java/us/ridiculousbakery/espressoexpress/EspressoApplication.java
@@ -0,0 +1,27 @@
+package us.ridiculousbakery.espressoexpress;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by teddywyly on 6/12/15.
+ */
+public class EspressoApplication extends Application {
+    public static final String YOUR_APPLICATION_ID = "Qdu88JcFCg97IGfVYvIXDCjZnMhocduPeRzDq3rN";
+    public static final String YOUR_CLIENT_KEY = "rTBgE3Js7XPDCcgZ2CFWx2mGo5XGF93uLGpNCtvy";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+        // Test Parse
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+}
