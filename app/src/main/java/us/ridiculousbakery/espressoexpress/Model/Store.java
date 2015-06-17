656b18565f896b5e21393d8dddb29f6d4ecf3245
Bert <bert.kuo@gmail.com>
2015-06-16 21:00:23 -0700

    stores

--- app/src/main/java/us/ridiculousbakery/espressoexpress/Model/Store.java
+++ app/src/main/java/us/ridiculousbakery/espressoexpress/Model/Store.java
@@ -1,9 +1,16 @@
 package us.ridiculousbakery.espressoexpress.Model;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bkuo on 6/3/15.
@@ -25,6 +32,7 @@ public class Store implements Serializable {
     }
 
     private StoreMenu storeMenu;
+
     public Integer getBackground() {
         return background;
     }
@@ -39,10 +47,12 @@ public class Store implements Serializable {
         this.background = background;
         this.lon = latlng.longitude;
         this.lat = latlng.latitude;
-        this.logo=logo;
-        this.storeMenu=menu;
+        this.logo = logo;
+        this.storeMenu = menu;
         this.orders = orders;
-        for(Order order : orders){order.setStore(this);}
+        for (Order order : orders) {
+            order.setStore(this);
+        }
     }
 
     public ArrayList<Order> getOrders() {
@@ -69,4 +79,39 @@ public class Store implements Serializable {
         return new LatLng(lat, lon);
     }
 
+    public static Store fromParseStore(ParseStore pstore) {
+
+    }
+
+    public static void StoresfromParse(LatLng ll, FindCallback<ParseStore> cb) {
+        ParseQuery<ParseStore> query = ParseQuery.getQuery("Store");
+        query.whereLessThan("lat", ll.latitude + .0250);
+        query.whereLessThan("lon", ll.longitude + 0.025);
+        query.whereGreaterThan("lat", ll.latitude - .0250);
+        query.whereGreaterThan("lon", ll.longitude - 0.025);
+        query.findInBackground(cb);
+    }
+
+//    new FindCallback<ParseStore>() {
+//        public void done(List<ParseStore> pstores, ParseException e) {
+//
+//            if (e == null) {
+//                ArrayList<Store> new_stores = new ArrayList<Store>();
+//                for (ParseStore ps : pstores) {
+//                    new_stores.add(fromParseStore(ps));
+//                }
+//
+//                Log.d("score", "Retrieved " + pstores.size() + " scores");
+//            } else {
+//                Log.d("parse store", "Error: " + e.getMessage());
+//            }
+//
+//        }
+//    }
+
+    static public ArrayList<Store> stores = new ArrayList<Store>();
+
+    static public class ParseStore extends ParseObject {
+
+    }
 }
