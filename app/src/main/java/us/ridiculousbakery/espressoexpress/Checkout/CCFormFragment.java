975d4016955fa5abc58d53a60de69827e3152106
Bert Kuo <bert@flywheel.com>
2015-06-23 10:27:20 -0700

    short cc form

--- app/src/main/java/us/ridiculousbakery/espressoexpress/Checkout/CCFormFragment.java
+++ app/src/main/java/us/ridiculousbakery/espressoexpress/Checkout/CCFormFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -76,12 +74,12 @@ public class CCFormFragment extends DialogFragment {
         if (getDialog() == null) {
             return;
         }
-        Display display = getActivity().getWindowManager().getDefaultDisplay();
-        DisplayMetrics displayMetrics  = new DisplayMetrics();
-        display.getMetrics(displayMetrics);
-        float dialogWidth = displayMetrics.widthPixels - 56; // specify a value here
-        float dialogHeight = displayMetrics.heightPixels - 56; // specify a value here
-        getDialog().getWindow().setLayout(Math.round(dialogWidth), Math.round(dialogHeight));
+//        Display display = getActivity().getWindowManager().getDefaultDisplay();
+//        DisplayMetrics displayMetrics  = new DisplayMetrics();
+//        display.getMetrics(displayMetrics);
+//        float dialogWidth = displayMetrics.widthPixels - 56; // specify a value here
+//        float dialogHeight = displayMetrics.heightPixels - 56; // specify a value here
+//        getDialog().getWindow().setLayout(Math.round(dialogWidth), Math.round(dialogHeight));
     }
 
     private void setupListeners() {
