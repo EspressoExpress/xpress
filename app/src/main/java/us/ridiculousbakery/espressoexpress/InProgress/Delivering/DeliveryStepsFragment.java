aaeced660712ebaf49683c17828983d9704660df
Bert <bert.kuo@gmail.com>
2015-06-24 15:35:40 -0700

    delivery stuff

--- app/src/main/java/us/ridiculousbakery/espressoexpress/InProgress/Delivering/DeliveryStepsFragment.java
+++ app/src/main/java/us/ridiculousbakery/espressoexpress/InProgress/Delivering/DeliveryStepsFragment.java
@@ -13,8 +13,13 @@ import us.ridiculousbakery.espressoexpress.R;
  * Created by bkuo on 6/24/15.
  */
 public class DeliveryStepsFragment extends Fragment {
-
+    private ViewGroup elAccepted;
+    private ViewGroup elPickedUp;
+    private ViewGroup elDelivered;
     private static DeliveryStepsFragment _instance;
+    private Integer iAccepted;
+    private Integer iPickedUp;
+    private Integer iDelivered;
 
     public static DeliveryStepsFragment instance(){
         if(_instance==null) _instance=new DeliveryStepsFragment();
@@ -24,11 +29,14 @@ public class DeliveryStepsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View _v = inflater.inflate(R.layout.delivery_fragment, null, false);
         ViewGroup v = (ViewGroup) _v;
+
+
         return v;
     }
 
